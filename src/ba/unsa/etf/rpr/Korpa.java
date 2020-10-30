05ffb87ecdf5eefa93c221a6adffbac0ebdbd092
nedim <https://github.com/nhosic1>
2020-10-30 15:28:11 +0100

    Dodani atributi i metode koje falile.

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] artikli = new Artikl[50];
+
+    public Korpa(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public Korpa() {
+
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+    public boolean dodajArtikl(Artikl artikl){
+        boolean dodan = false;
+        for (int i=0; i<50; i++){
+            if (artikli[i] == null){
+                artikli[i] = artikl;
+                dodan = true;
+                break;
+            }
+        }
+        return dodan;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a = null;
+        for (int i=0; i<50; i++){
+            if (artikli[i] != null && artikli[i].getKod().equals(kod)){
+                a = artikli[i];
+                artikli[i] = null;
+                break;
+            }
+        }
+        return a;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for (int i=0; i<50; i++){
+            if (artikli[i] != null){
+                suma = suma + artikli[i].getCijena();
+            }
+        }
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl[] artikli = new Artikl[1000];
+
+    public Supermarket(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public Supermarket() {
+
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+    public void dodajArtikl(Artikl artikl){
+        for (int i=0; i<1000; i++){
+            if (artikli[i] == null){
+                artikli[i] = artikl;
+                break;
+            }
+        }
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a = null;
+        for (int i=0; i<1000; i++){
+            if (artikli[i] != null && artikli[i].getKod().equals(kod)){
+                a = artikli[i];
+                artikli[i] = null;
+                break;
+            }
+        }
+        return a;
+    }
 }
